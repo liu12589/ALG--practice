2dcfb1333a9b8952bf643ecbf26620a314a98743
lmy <1910260504@qq.com>
2022-06-26 10:57:04 +0800

    【固定窗口、双指针】

--- 字符串/567PermutationString.go
+++ 字符串/567PermutationString.go
@@ -4,9 +4,9 @@ import "fmt"
 
 // 使用滑动窗口
 func main() {
-	s1 := "adc"
-	s2 := "dcda"
-	fmt.Println(checkInclusion(s1, s2))
+	s1 := "hello"
+	s2 := "ooolleoooleh"
+	fmt.Println(checkInclusion1(s1, s2))
 }
 
 // 固定长度滑动窗口
@@ -33,6 +33,39 @@ func checkInclusion(s1 string, s2 string) bool {
 		}
 		if end-start+1 == lengthS1 {
 			return true
+		}
+	}
+	return false
+}
+
+func checkInclusion1(s1 string, s2 string) bool {
+	lengthS1 := len(s1)
+	lengthS2 := len(s2)
+	needLength := lengthS1 // 记录固定窗口的大小
+	if lengthS1 > lengthS2 {
+		return false
+	}
+	var s1Map = make(map[byte]int, lengthS2)
+	for i := range s1 {
+		s1Map[s1[i]]++
+	}
+	var start = 0
+	// 使用固定窗口，以start的遍历为依据
+	for end := range s2 {
+		if s1Map[s2[end]] > 0 {
+			needLength--
+		}
+		s1Map[s2[end]]--
+		// 长度一致，先判断是否符合条件，符合条件直接返回。不符合条件start向右移动一位
+		if end-start+1 == lengthS1 {
+			if needLength == 0 {
+				return true
+			}
+			if s1Map[s2[start]] >= 0 {
+				needLength++
+			}
+			s1Map[s2[start]]++
+			start++
 		}
 	}
 	return false
